3bde27f9ebf7cdf8d45a91be015bb30dfd09e9ab
SushGit07 <sushant.patil0777@gmail.com>
2025-07-02 22:56:24 +0530

    first commit

---
+++ jambavanta.js
@@ -0,0 +1,15 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+function navigateToDonateNow() {
+    window.location.href = "project_donate.html";
+}
+
+function navigateToHomePage() {
+    window.location.href = "project_home.html";
+}
+
