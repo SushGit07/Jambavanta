f1ed54e2a2a7f5c5e8074e3ab5a88cac29ec7a59
SushGit07 <sushant.patil0777@gmail.com>
2025-07-07 11:35:22 +0530

    scroll Top Button Changes

--- jambavanta.js
+++ jambavanta.js
@@ -13,3 +13,17 @@ function navigateToHomePage() {
     window.location.href = "project_home.html";
 }
 
+// Show/hide button on scroll
+window.onscroll = function () {
+    const btn = document.getElementById("scrollTopBtn");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+};
+
+// Scroll to top function
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
